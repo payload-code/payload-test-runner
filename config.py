a8c85a0836d119d6b2888bb65938c45beed430b2
Nathan Yearout <nathan@payload.co>
2024-08-07 14:32:16 -0400

    Add env variables

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ CONFIG_FILE = "config.json"
 
 DOCKER_COMPOSE_CMD = os.environ.get('DOCKER_COMPOSE_CMD', 'docker-compose')
 
+REBUILD_DB_BEFORE_TESTS = os.environ.get('REBUILD_DB_BEFORE_TESTS', 'false').lower() == 'true'
+YARN_REBUNDLE_BEFORE_TESTS = os.environ.get('YARN_REBUNDLE_BEFORE_TESTS', 'false').lower() == 'true'
